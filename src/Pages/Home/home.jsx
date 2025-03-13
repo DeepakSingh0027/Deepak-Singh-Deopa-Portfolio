8f3369c8fd35f00c5d41ad3e6a69bd8c25f30854
DeepakSingh0027 <149071400+DeepakSingh0027@users.noreply.github.com>
2025-03-13 22:45:04 +0530

    Enhance Home Page with Animated Headings and Google Fonts

--- src/Pages/Home/home.jsx
+++ src/Pages/Home/home.jsx
@@ -5,6 +5,7 @@ import { motion } from "framer-motion";
 import { Hand, ThumbsUp } from "lucide-react";
 import BlurText from "./../../animations/BlurText";
 import TrueFocus from "./../../animations/TrueFocus";
+
 const Home = () => {
   const [isHello, setHello] = useState(false);
   const { theme, toggleTheme, position, setPos } = useContext(ThemeContext);
@@ -21,6 +22,7 @@ const Home = () => {
     <div className="mt-42 px-10">
       <div>
         <div>
+          {/*Hey text */}
           <div className="flex space-x-3 justify-center">
             {isHello ? (
               <ThumbsUp className="cursor-pointer w-7 h-7 text-green-500 animate-thumb" />
@@ -56,12 +58,71 @@ const Home = () => {
               />
             )}
           </div>
-          <div className="flex space-x-3 justify-center"></div>
-          <div className="flex justify-center "></div>
+          {/*Heading */}
+          <div className="relative mt-16 font-bold">
+            {/* First Line (Text Moves Left) */}
+            <div className="relative mb-4">
+              <motion.p
+                initial={{ x: 100, opacity: 0 }}
+                whileInView={{ x: 0, opacity: 1 }}
+                transition={{ duration: 1, ease: "easeInOut" }}
+                viewport={{ amount: 0.5 }}
+                className={`relative mx-16 z-10 text-7xl mix-blend-difference w-fit ${
+                  theme === "light" ? "text-[#b1b1b1]" : "text-[#FFFFF0]"
+                }`}
+              >
+                Turning <span className="text-red-500">Ideas</span>
+              </motion.p>
+              <div
+                className={`absolute inset-0 -z-10 top-1/2 h-0.5 w-full ${
+                  theme === "light" ? "bg-gray-800" : "bg-gray-400"
+                }`}
+              ></div>
+            </div>
 
-          <div className="text-[#f5fffa]"></div>
+            {/* Second Line (Text Stays in Place) */}
+            <div className="relative mb-4 pt-6">
+              <motion.p
+                initial={{ opacity: 0 }}
+                whileInView={{ opacity: 1 }}
+                transition={{ duration: 1.2, ease: "easeInOut" }}
+                viewport={{ amount: 0.5 }}
+                className={`relative mx-16 z-10 text-7xl text-center mix-blend-difference ${
+                  theme === "light" ? "text-[#b1b1b1]" : "text-[#FFFFF0]"
+                }`}
+              >
+                Into
+              </motion.p>
+              <div
+                className={`absolute inset-0 mt-4 top-1/2 h-0.5 w-full ${
+                  theme === "light" ? "bg-gray-800" : "bg-gray-400"
+                }`}
+              ></div>
+            </div>
+
+            {/* Third Line (Text Moves Right) */}
+            <div className="relative pt-6 flex justify-end">
+              <motion.p
+                initial={{ x: -100, opacity: 0 }}
+                whileInView={{ x: 0, opacity: 1 }}
+                transition={{ duration: 1, ease: "easeInOut" }}
+                viewport={{ amount: 0.5 }}
+                className={`relative z-10 text-7xl mx-5 mix-blend-difference w-fit ${
+                  theme === "light" ? "text-[#b1b1b1]" : "text-[#FFFFF0]"
+                }`}
+              >
+                <span className="text-red-500">Scalable</span> Solutions.
+              </motion.p>
+              <div
+                className={`absolute inset-0 mt-4 top-1/2 h-0.5 w-full ${
+                  theme === "light" ? "bg-gray-800" : "bg-gray-400"
+                }`}
+              ></div>
+            </div>
+          </div>
         </div>
-        {/*part-2*/}
+
+        {/*About Me*/}
         <div></div>
       </div>
     </div>
